f00ec3094dc21d34d3f8a3d82c69622ce6d1550f
Satria Bayuaji <satria_26@apps.ipb.ac.id>
2019-10-30 01:25:10 +0700

    Dialogue Manager

---
+++ Assets/Hilang.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hilang : MonoBehaviour
+{
+    public GameObject hilang, muncul;
+
+    public void buttonHilang()
+    {
+        hilang.SetActive(false);
+        muncul.SetActive(true);
+    }
+}
